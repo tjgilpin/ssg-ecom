2351fdd11885bf4147a1c1d6faaab2355239c734
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2024-07-21 21:23:54 +0100

    Use STRIPE_TEST_KEY for prod

--- src/_data/prices.js
+++ src/_data/prices.js
@@ -1,7 +1,7 @@
 const environment = process.env.CONTEXT
 const api_key = environment !== "production"
   ? process.env.STRIPE_TEST_KEY
-  : process.env.STRIPE_SECRET_KEY;
+  : process.env.STRIPE_TEST_KEY;
 const stripe = require("stripe")(api_key);
 
 // One product can have multiple prices but for my use case, it's 1:1 so this works without having to massage the data
